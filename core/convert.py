74862ca41894d3b5b25fd1ba0d5ff26492c230c9
furusato <ichiro.furusato@gmail.com>
2021-11-16 08:26:24 +1300

    beginnings of ICM20948 IMU support

---
+++ core/convert.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2021 by Murray Altheim. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Murray Altheim
+# created:  2020-03-27
+# modified: 2020-03-27
+#
+#  A class containing some static IMU-related conversion methods.
+#
+
+import numpy, math
+from math import pi as PI
+
+X = 0
+Y = 1
+Z = 2
+AXES = Y, Z
+
+# ..............................................................................
+class Convert:
+
+    # ..........................................................................
+    @staticmethod
+    def to_degrees(radians):
+        return math.degrees(radians)
+
+    # ..........................................................................
+    @staticmethod
+    def to_radians(degrees):
+        return math.radians(degrees)
+
+    # ..........................................................................
+    @staticmethod
+    def rps_to_dps(rps):
+        return rps * 57.29578 
+
+    # ..............................................................................
+    @staticmethod
+    def offset_in_degrees(angle, offset):
+        '''
+        Add two angles (provided in degrees), returning the result.
+        '''
+        return ( angle + offset ) % 360.0
+
+    # ..............................................................................
+    @staticmethod
+    def difference_in_degrees(angle1, angle2):
+        '''
+        Returns the shortest angular difference between two angles (provided
+        in degrees). When the difference is 180° a positive angle is returned.
+        '''
+#       x = math.radians(angle2)
+#       y = math.radians(angle1)
+#       return math.degrees(min(y-x, y-x+2 * math.pi, y-x-2 * math.pi, key=abs))
+        offset = ( angle1 - angle2 ) % 360.0
+        return offset - 360.0 if offset > 180.0 else offset
+
+    # ..............................................................................
+    @staticmethod
+    def offset_in_radians(angle, offset):
+        '''
+        Add two angles (provided in radians), returning the result.
+        '''
+        return (angle + offset) % (2.0 * PI)
+
+    # ..........................................................................
+    @staticmethod
+    def quaternion_to_euler_angle(w, x, y, z):
+        q = Quaternion(w, x, y, z)
+        deg = q.degrees
+        return deg
+
+    # ..........................................................................
+    @staticmethod
+    def quaternion_to_euler(w, x, y, z):
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll = math.atan2(t0, t1)
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch = math.asin(t2)
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        heading = math.atan2(t3, t4)
+        return [heading, pitch, roll]
+
+    # ..........................................................................
+    @staticmethod
+    def heading_from_magnetometer(amin, amax, mag):
+        '''
+        :param amin:  the original list of magnetometer readings used as a minimum
+        :param amax:  the original list of magnetometer readings used as a maximum
+        :param mag:   the magnetometer reading (x, y, z) to convert to a heading
+        '''
+        mag = list(mag)
+        for i in range(3):
+            v = mag[i]
+            if v < amin[i]:
+                amin[i] = v
+            if v > amax[i]:
+                amax[i] = v
+            mag[i] -= amin[i]
+            try:
+                mag[i] /= amax[i] - amin[i]
+            except ZeroDivisionError:
+                pass
+            mag[i] -= 0.5
+    
+        heading = math.atan2(mag[AXES[0]], mag[AXES[1]])
+        if heading < 0:
+            heading += 2 * math.pi
+        heading = math.degrees(heading)
+        heading = int(round(heading))
+        return heading
+
+    # ..........................................................................
+    @staticmethod
+    def quaternion_to_euler_angle_other(w, x, y, z):
+        ysqr = y * y
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + ysqr)
+        X = numpy.degrees(numpy.arctan2(t0, t1))
+        t2 = +2.0 * (w * y - z * x)
+        t2 = numpy.clip(t2, a_min=-1.0, a_max=1.0)
+        Y = numpy.degrees(numpy.arcsin(t2))
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (ysqr + z * z)
+        Z = numpy.degrees(numpy.arctan2(t3, t4))
+        return X, Y, Z
+
+    # ..........................................................................
+    @staticmethod
+    def convert_to_euler(qw, qx, qy, qz):
+        # can get the euler angles back out in degrees (set to True)
+        _euler = quat2euler(qw, qx, qy, qz, degrees=True)
+        _heading = -1.0 * _euler[2]
+        _pitch   = _euler[1]
+        _roll    = -1.0 * _euler[0]
+        return [ _heading, _pitch, _roll ]
+
+    # ..........................................................................
+    @staticmethod
+    def convert_to_degrees(x, y, z):
+        '''
+        Provided a x,y,z magnetometer reading returns a heading value in degrees.
+    
+        source:  https://cdn-shop.adafruit.com/datasheets/AN203_Compass_Heading_Using_Magnetometers.pdf
+        '''
+        if y == 0.0:
+            if x < 0.0:
+                return 180.0
+            elif x >= 0.0:
+                return 0.0
+        elif y > 0.0:
+            return 90.0 - math.atan( x / y ) * ( 180.0 / math.pi )
+        elif y < 0.0:
+            return 270.0 - math.atan( x / y ) * ( 180.0 / math.pi )
+
+    # ..........................................................................
+    @staticmethod
+    def rotate_90_degrees(degrees):
+        ''' 
+        Return the argument rotated 90 degrees.
+        '''
+        return Convert.to_degrees(Convert.to_radians(degrees) - ( math.pi / 2.0 )) 
+
+    # ..........................................................................
+    @staticmethod
+    def in_range(p, q, error_range):
+        '''
+        Returns True if the first two numbers are within the supplied range
+        of each other.
+        '''
+        return p >= ( q - error_range ) and p <= ( q + error_range )
+
+# EOF
---
+++ icm20948_test.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010-2021 by Murray Altheim. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Murray Altheim
+# created:  2019-08-03
+# modified: 2021-11-15
+#
+#      X        
+#      ^
+#  Y < Z    _______________
+#           |             |
+#           |  ICM 20948  |
+#           |             |
+#           |_           _|
+#             |_I_I_I_I_|
+#
+
+import sys, time, traceback, math
+from colorama import init, Fore, Style
+init(autoreset=True)
+
+try:
+    from icm20948 import ICM20948
+except ImportError as ie:
+    sys.exit("This script requires the icm20948 module.\n"\
+           + "Install with: pip3 install --user icm20948")
+
+from core.logger import Level, Logger
+from core.config_loader import ConfigLoader
+from core.convert import Convert
+
+#X = 0
+#Y = 1
+#Z = 2
+#AXES = Y, Z
+
+# ..............................................................................
+class IMU():
+    '''
+        An ICM20948-based Inertial Measurement Unit (IMU).
+    '''
+    def __init__(self, config, level):
+        super().__init__()
+        self._log = Logger('imu', level)
+        if config is None:
+            raise ValueError('no configuration provided.')
+#       _config = config['kros'].get('imu')
+        self._icm20948 = ICM20948(i2c_addr=0x69)
+        self._amin = list(self._icm20948.read_magnetometer_data())
+        self._amax = list(self._icm20948.read_magnetometer_data())
+        self._log.info('amin: {}; amax: {}'.format(type(self._amin), type(self._amax)))
+        self._log.info('ready.')
+
+    def read_magnetometer(self):
+        return self._icm20948.read_magnetometer_data()
+
+    def read_accelerometer_gyro(self):
+        return self._icm20948.read_accelerometer_gyro_data()
+
+#    def heading_from_magnetometer(self, mag):
+#        mag = list(mag)
+#        for i in range(3):
+#            v = mag[i]
+#            if v < self._amin[i]:
+#                self._amin[i] = v
+#            if v > self._amax[i]:
+#                self._amax[i] = v
+#            mag[i] -= self._amin[i]
+#            try:
+#                mag[i] /= self._amax[i] - self._amin[i]
+#            except ZeroDivisionError:
+#                pass
+#            mag[i] -= 0.5
+#    
+#        heading = math.atan2(mag[AXES[0]], mag[AXES[1]])
+#        if heading < 0:
+#            heading += 2 * math.pi
+#        heading = math.degrees(heading)
+#        heading = int(round(heading))
+#        return heading
+
+def get_accel_color(value):
+    if value < -1.0:
+        return Fore.RED
+    elif value > 1.0:
+        return Fore.GREEN
+    else:
+        return Fore.YELLOW
+
+def get_gyro_color(value):
+    if value < -30.0:
+        return Fore.RED
+    elif value > 30.0:
+        return Fore.GREEN
+    else:
+        return Fore.YELLOW
+
+def get_mag_color(value):
+    if value < -30.0:
+        return Fore.RED
+    elif value > 30.0:
+        return Fore.GREEN
+    else:
+        return Fore.YELLOW
+
+# main .........................................................................
+def main(argv):
+
+    _GYRO_ONLY  = False
+    _ACCEL_ONLY = False
+    _MAG_ONLY   = True
+
+    try:
+
+        # read YAML configuration
+        _config = ConfigLoader().configure()
+
+        _imu = IMU(_config, Level.INFO)
+
+        while True:
+#           ax, ay, az, gx, gy, gz = _imu.read_accelerometer_gyro()
+            if _GYRO_ONLY:
+                acc = _imu.read_accelerometer_gyro()
+                _x = acc[3] 
+                _y = acc[4] 
+                _z = acc[5]
+                print(Fore.CYAN + 'Gyro:\t' 
+                        + get_gyro_color(_x) + '{:05.2f}\t'.format(_x)
+                        + get_gyro_color(_y) + '{:05.2f}\t'.format(_y)
+                        + get_gyro_color(_z) + '{:05.2f}\t'.format(_z) )
+            elif _ACCEL_ONLY:
+                acc = _imu.read_accelerometer_gyro()
+                _x = acc[0] 
+                _y = acc[1] 
+                _z = acc[2]
+                print(Fore.CYAN + 'Accel:\t' 
+                        + get_accel_color(_x) + '{:05.2f}\t'.format(_x)
+                        + get_accel_color(_y) + '{:05.2f}\t'.format(_y)
+                        + get_accel_color(_z) + '{:05.2f}\t'.format(_z) )
+            elif _MAG_ONLY:
+                mag = _imu.read_magnetometer()
+                _x = mag[0] 
+                _y = mag[1] 
+                _z = mag[2]
+                print(Fore.CYAN + 'Mag:\t' 
+                        + get_mag_color(_x) + '{:05.2f}\t'.format(_x)
+                        + get_mag_color(_y) + '{:05.2f}\t'.format(_y)
+                        + get_mag_color(_z) + '{:05.2f}\t'.format(_z) )
+            else:
+                acc = _imu.read_accelerometer_gyro()
+#               x, y, z = _imu.read_magnetometer()
+                mag = _imu.read_magnetometer()
+                heading = Convert.heading_from_magnetometer(_imu._amin, _imu._amax, mag)
+                print(Fore.CYAN    + 'Accel: {:05.2f} {:05.2f} {:05.2f} '.format(acc[0], acc[1], acc[2]) \
+                    + Fore.YELLOW  + '\tGyro: {:05.2f} {:05.2f} {:05.2f} '.format(acc[3], acc[4], acc[5]) \
+                    + Fore.MAGENTA + '\tMag: {:05.2f} {:05.2f} {:05.2f}  '.format(mag[0], mag[1], mag[2]) \
+                    + Fore.GREEN   + '\tHeading: {:d}°'.format(heading))
+            time.sleep(0.25)
+
+    except KeyboardInterrupt:
+        print(Fore.CYAN + Style.BRIGHT + 'caught Ctrl-C; exiting...')
+        
+    except Exception:
+        print(Fore.RED + Style.BRIGHT + 'error starting imu: {}'.format(traceback.format_exc()))
+
+# call main ....................................................................
+if __name__== "__main__":
+    main(sys.argv[1:])
+
+#EOF
